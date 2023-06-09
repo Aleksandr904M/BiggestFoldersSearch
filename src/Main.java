0ac66da9184980d8baf1bd85068561ed6b6216d1
Aleksandr Mankov <manurogan@yandex.ru>
2023-06-09 21:14:07 +0500

    div

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
+import java.io.File;
+
 public class Main {
+    public static void main(String[] args) {
+
+        String folderPath = "C:\\Java\\записки Java"; //строка с указанием пути к файлу
+        File file = new File(folderPath);
+
+        System.out.println(getFolderSize(file)); //размер ненастоящий, это размер блока данных на все ссылки в папке
+    }
+
+    public static long getFolderSize (File folder) { //рекурсивная функция
+
+        if (folder.isFile()) {       //если folder является файлом
+            return folder.length();  //вернуть размер файла
+        }
+        //если folder - папка
+        long sum = 0;                //переменная для суммы
+        File [] files = folder.listFiles();//получить список файлов в этой папке
+        for (File file : files) {    //циклом идем по списку
+            sum += getFolderSize(file);
+        } return sum;
+
+    }
 }
